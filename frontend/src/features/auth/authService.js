3dd522d5c9b255637356f1c6ef7b48e806bcb218
Sherkhan K <sherkhan4work@gmail.com>
2023-04-14 18:00:53 +0600

    Call to the backend, mediator twin front and back

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,22 @@
+// The authService is a fetch/axios call to the backend
+import axios from 'axios'
+
+// Put:  "proxy": "http://localhost:5000", in the frontend package.json, when in dev, so it will get backend url
+const API_URL = '/api/users'
+
+// Register user
+const register = async (userData) => {
+    // Same thing as in postman, passing data to backend
+    const response = await axios.post(API_URL, userData)
+
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+    return response.data
+}
+
+const authService = {
+    register
+}
+
+export default authService
