579a16817474686110c61c05456662bbfc2ea489
Sherkhan K <sherkhan4work@gmail.com>
2023-04-14 17:59:33 +0600

    explanation of a file

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,3 +1,4 @@
+// The store.js is just housing the state so you have access to it anywhere in your application
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
 
