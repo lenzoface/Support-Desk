e8c6982675288b6cd00a3f918239b2fac37c4ba4
Sherkhan K <sherkhan4work@gmail.com>
2023-04-14 18:01:52 +0600

    Reducers/actions to use across diff pages

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,18 +1,84 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
+
+// Get user from localstorage, so it won't log off user when reloading a page
+const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-    user: null,
-    isError: false,
-    isSuccess: false,
-    isLoading: false,
-    message: ''
-}
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+// Register new user, get data from the form
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+        // Get message from backend
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+        // Put message in frontend payload
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Login a user, get data from the form
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+
+    return thunkAPI.rejectWithValue(message);
+  }
+});
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {},
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder) => {
+    // cases when some action is dispatched (can look in redux devtool). allows us to add cases and chain on
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload; //*this is coming from the try in register
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.user = null;
+        // payload comes from thunkAPI
+        state.message = action.payload; //* this is coming from the trycatch error message from backend see return thunkAPI.rejectWithValue(message) in const register
+      });
+  },
+});
 
-export default authSlice.reducer
+// We need to export reducers actions for it to pass into JSX files
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
