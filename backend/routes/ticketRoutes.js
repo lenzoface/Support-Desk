cf8773e1e0e49bd6d31d537f9c3dff3c1e4c2e82
Sherkhan K <sherkhan4work@gmail.com>
2023-04-15 16:02:48 +0600

    Tickets backend functionality setup

---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getTickets,
+  createTicket,
+} = require("../controllers/ticketController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+// We can chain on route methods instead of writing them one by one
+router.route("/").get(protect, getTickets).post(protect, createTickets);
+
+module.exports = router;
