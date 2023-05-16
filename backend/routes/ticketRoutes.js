a1cffa3debe9897abffc9bcf66ec118af5950405
Sherkhan K <sherkhan4work@gmail.com>
2023-05-16 10:56:57 +0600

    full CRUD, backend

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,12 +2,17 @@ const express = require("express");
 const router = express.Router();
 const {
   getTickets,
+  getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 } = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 // We can chain on route methods instead of writing them one by one
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router.route('/:id').get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket)
 
 module.exports = router;
