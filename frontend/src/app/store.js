a9282a21c72e63adcdd81f5afae5bfdb2f10eac1
Sherkhan K <sherkhan4work@gmail.com>
2023-05-16 13:13:58 +0600

    add tickets to redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 // The store.js is just housing the state so you have access to it anywhere in your application
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
+import ticketReducer from '../features/tickets/ticketSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    ticket: ticketReducer,
   },
 });
