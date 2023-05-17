a0b8dc4b88dff0e58ffaf8bd26f12af9c2076776
Sherkhan K <sherkhan4work@gmail.com>
2023-05-17 10:43:01 +0600

    Create ticket functionality

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -1,25 +1,94 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-import ticketService from './ticketService'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import ticketService from "./ticketService";
 
 const initialState = {
-    tickets: [],
-    ticket: {},
-    isError: false,
-    isSuccess: false,
-    isLoading: false,
-    message: ''
-}
+  tickets: [],
+  ticket: {},
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+// Create new ticket, get data from the form
+export const createTicket = createAsyncThunk(
+  "tickets/create",
+  async (ticketData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      // Get message from backend/API
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      // Put message in frontend payload
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Get user tickets, get data from the form
+export const getTickets = createAsyncThunk(
+  "tickets/getAll",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.getTickets(token);
+    } catch (error) {
+      // Get message from backend/API
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      // Put message in frontend payload
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const ticketSlice = createSlice({
-    name: 'ticket',
-    initialState,
-    reducers: {
-        reset: (state) => initialState
-    },
-    extraReducers: (builer) => {
-        // case
-    }
-})
+  name: "ticket",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTicket.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createTicket.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+      })
+      .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload; // Comes from line 30
+      })
+      .addCase(getTickets.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getTickets.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.tickets = action.payload
+      })
+      .addCase(getTickets.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload; // Comes from line 30
+      });
+  },
+});
 
-export const {reset} = ticketSlice.actions
-export default ticketSlice.reducer
+export const { reset } = ticketSlice.actions;
+export default ticketSlice.reducer;
