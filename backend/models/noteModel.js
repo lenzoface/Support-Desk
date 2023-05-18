8ca2c9d99ce81727375a1b67103ffca36cca3357
Sherkhan K <sherkhan4work@gmail.com>
2023-05-18 12:24:53 +0600

    Notes backend

---
+++ backend/models/noteModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const noteSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Ticket",
+    },
+    text: {
+      type: String,
+      required: [true, "Please add some text"],
+    },
+    // to be done in future, make a portal for staff to manage tickets
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+    staffId: {
+      type: String,
+    },
+  },
+  // Will have info about actions (time, date)
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Note", noteSchema);
