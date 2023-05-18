fef46ac9a5f48751a14cfa8e89f3a20417544c69
Sherkhan K <sherkhan4work@gmail.com>
2023-05-18 12:24:45 +0600

    Re-route into note router

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -10,6 +10,10 @@ const {
 
 const { protect } = require("../middleware/authMiddleware");
 
+// Re-route into note router
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 // We can chain on route methods instead of writing them one by one
 router.route("/").get(protect, getTickets).post(protect, createTicket);
 
